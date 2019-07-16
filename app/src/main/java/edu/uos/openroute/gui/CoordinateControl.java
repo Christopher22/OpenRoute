852a25f203acb019e1faced35480c597328364aa
Christopher Gundler <c.gundler@mail.de>
2019-07-16 11:34:01 +0200

    Improve documentation

--- app/src/main/java/edu/uos/openroute/gui/CoordinateControl.java
+++ app/src/main/java/edu/uos/openroute/gui/CoordinateControl.java
@@ -7,36 +7,62 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GoogleApiAvailability;
 import edu.uos.openroute.R;
 import edu.uos.openroute.util.LocationAccess;
 import edu.uos.openroute.util.Position;
 
 import java.text.NumberFormat;
 
+/**
+ * A wrapper around inputs used to enter a position.
+ */
 public class CoordinateControl {
 
-    private EditText latitude, longitude;
+    private final EditText latitude, longitude;
 
+    /**
+     * Apply the wrapper on an existing widgets.
+     *
+     * @param parent                     The activity.
+     * @param latitudeId                 The text input for the latitude.
+     * @param longitudeId                The text input for the longitude.
+     * @param setCurrentLocationButtonId The button for setting both text inputs to the current position.
+     */
     public CoordinateControl(Activity parent, int latitudeId, int longitudeId, int setCurrentLocationButtonId) {
         this.latitude = parent.findViewById(latitudeId);
         this.longitude = parent.findViewById(longitudeId);
 
-        if (ConnectionResult.SUCCESS == GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(parent)) {
+        // Show the button for setting both text inputs to the current position iff the required services are available.
+        if (LocationAccess.isAvailable(parent)) {
             Button setCurrentLocationButton = parent.findViewById(setCurrentLocationButtonId);
             setCurrentLocationButton.setVisibility(View.VISIBLE);
             setCurrentLocationButton.setOnClickListener(new SetLocationOperation(parent, latitude, longitude));
         }
     }
 
+    /**
+     * Return the position entered by the user.
+     *
+     * @return the position or 'null' if it is invalid.
+     */
     public Position getPosition() {
         return Position.fromString(this.latitude.getText().toString(), this.longitude.getText().toString());
     }
 
+    /**
+     * An asychronious operation for setting the text inputs to the current position.
+     */
     private static class SetLocationOperation extends LocationAccess {
+
         private final EditText latitude, longitude;
 
+        /**
+         * Create the operation which is started on click.
+         *
+         * @param activity  The activity.
+         * @param latitude  The text input for the latitude.
+         * @param longitude The text input for the longitude.
+         */
         public SetLocationOperation(Activity activity, EditText latitude, EditText longitude) {
             super(activity);
             this.latitude = latitude;
@@ -45,11 +71,13 @@ public class CoordinateControl {
 
         @Override
         public void onSuccess(Location location) {
+            // Show error if no valid location was provided by Android.
             if (location == null) {
                 Toast.makeText(this.activity, R.string.location_not_available, Toast.LENGTH_LONG).show();
                 return;
             }
 
+            // Format the resulting position accordingly to the decimal layout of the user language.
             NumberFormat formatter = NumberFormat.getInstance();
             this.longitude.setText(formatter.format(location.getLongitude()));
             this.latitude.setText(formatter.format(location.getLatitude()));
@@ -57,7 +85,10 @@ public class CoordinateControl {
 
         @Override
         public void onFailure(@NonNull Exception e) {
+            // Get the exact reason for the missing rights
             LocationAccess.MissingRightsException missingRights = (LocationAccess.MissingRightsException) e;
+
+            // Try to convince the user to grand the rights if he had not denied them permanently.
             if (!missingRights.isFinalDecision()) {
                 missingRights.showExplanation(R.string.permission_reason_title, R.string.permission_reason_text, R.string.yes, R.string.no);
             } else {
--- app/src/main/java/edu/uos/openroute/util/LocationAccess.java
+++ app/src/main/java/edu/uos/openroute/util/LocationAccess.java
@@ -3,58 +3,91 @@ package edu.uos.openroute.util;
 import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.SparseArray;
 import android.view.View;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-
+/**
+ * This class represents a access of the user location.
+ * Once started (by default on click), it handles all the permission management and fire the location asynchronously if possible.
+ */
 public abstract class LocationAccess implements View.OnClickListener, OnSuccessListener<android.location.Location>, OnFailureListener {
 
     private static SparseArray<LocationAccess> locationOperations;
     protected final Activity activity;
     private final FusedLocationProviderClient locationProvider;
     private int id;
+
+    /**
+     * Create a new access operation.
+     *
+     * @param activity The parent activity.
+     */
     public LocationAccess(Activity activity) {
         this.activity = activity;
         this.locationProvider = new FusedLocationProviderClient(activity);
 
+        // Set an unique id to this specific operation and save it in a global list
         this.id = LocationAccess.getOperations().size();
         LocationAccess.getOperations().append(this.id, this);
     }
 
+    /**
+     * Check if the required services are available.
+     *
+     * @param context The context of the call.
+     * @return true iff location access is available.
+     */
+    public static boolean isAvailable(Context context) {
+        return GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(context) == ConnectionResult.SUCCESS;
+    }
+
+    /**
+     * Handle the global response towards a permission request.
+     * WARNING: You need to call this function in 'onRequestPermissionsResult' of the current activity.
+     *
+     * @param requestCode  The request code.
+     * @param permissions  The permissions.
+     * @param grantResults The granted results.
+     */
     public static void handlePermissionRequest(int requestCode, String[] permissions, int[] grantResults) {
+        // Find the operation which was requesting the position.
         LocationAccess operation = LocationAccess.getOperations().get(requestCode);
-        if (permissions[0].equals(Manifest.permission.ACCESS_FINE_LOCATION) && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            operation.getLocation();
-        } else {
-            operation.onFailure(new MissingRightsException(operation));
+
+        // Search for the exact requested right ...
+        for (int i = 0; i < permissions.length; ++i) {
+            if (!permissions[i].equals(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                continue;
+            }
+
+            // ... and get the location if it was grated, otherwise report failure.
+            if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                operation.getLocation();
+            } else {
+                operation.onFailure(new MissingRightsException(operation));
+            }
         }
     }
 
+    /**
+     * A singletone for a global list of operations dealing with locations.
+     *
+     * @return existing operations.
+     */
     private static SparseArray<LocationAccess> getOperations() {
         if (LocationAccess.locationOperations == null) {
             LocationAccess.locationOperations = new SparseArray<>();
         }
         return LocationAccess.locationOperations;
-    }
-
-    private void getLocation() {
-        try {
-            this.locationProvider.getLastLocation().addOnSuccessListener(this);
-        } catch (SecurityException ex) {
-            this.onFailure(new MissingRightsException(this));
-        }
-    }
-
-    private void requestPermissions() {
-        final String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
-        ActivityCompat.requestPermissions(activity, permissions, this.id);
     }
 
     @Override
@@ -66,20 +99,62 @@ public abstract class LocationAccess implements View.OnClickListener, OnSuccessL
         }
     }
 
+    /**
+     * Get the location asynchronously. Once available, the OnSuccessListener callback will be called.
+     */
+    private void getLocation() {
+        try {
+            this.locationProvider.getLastLocation().addOnSuccessListener(this);
+        } catch (SecurityException ex) {
+            this.onFailure(new MissingRightsException(this));
+        }
+    }
+
+    /**
+     * Request the required permissions.
+     */
+    private void requestPermissions() {
+        final String[] permissions = new String[]{Manifest.permission.ACCESS_FINE_LOCATION};
+        ActivityCompat.requestPermissions(activity, permissions, this.id);
+    }
+
+    /**
+     * An exception which is thrown on missing rights for the operation.
+     */
     public static class MissingRightsException extends Exception {
+
         private final boolean finalDecision;
         private final LocationAccess access;
 
+        /**
+         * Creates a new exception.
+         *
+         * @param access The parent emitting the exception.
+         */
         private MissingRightsException(LocationAccess access) {
             super("The user did not granded the required permissions.");
+
             this.access = access;
             this.finalDecision = !ActivityCompat.shouldShowRequestPermissionRationale(access.activity, Manifest.permission.ACCESS_FINE_LOCATION);
         }
 
+        /**
+         * Check, if the user has denied the rights permanently.
+         *
+         * @return true iff the user has denied the rights permanently.
+         */
         public boolean isFinalDecision() {
             return finalDecision;
         }
 
+        /**
+         * Show a message to the users explaining why the rights are required and provide a way of changing theri minds.
+         *
+         * @param title    The string id of the title.
+         * @param messages The string id of the message.
+         * @param yes      The string id of the yes, if the user has changed his/her mind.
+         * @param no       The string id of the no.
+         */
         public void showExplanation(int title, int messages, int yes, int no) {
             new AlertDialog.Builder(access.activity)
                     .setTitle(title)
@@ -87,6 +162,7 @@ public abstract class LocationAccess implements View.OnClickListener, OnSuccessL
                     .setPositiveButton(yes, new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
+                            // Request permissions again
                             access.requestPermissions();
                         }
                     })
